bdbd25af95b5aebe8f2f161eae0d5adb05e7b00a
Koratprince <princekorat478@gmail.com>
2026-09-10 10:10:36 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,24 +1,17 @@
 #include <stdio.h>
 
-int main()
-{
-    int basic, hra, da, allowance, gross;
+int main() {
+    long total_seconds;
+    int hours, minutes, seconds;
 
-    printf("Enter Basic Salary: ");
-    scanf("%d", &basic);
+    printf("Enter total seconds: ");
+    scanf("%ld", &total_seconds);
 
-    printf("Enter HRA: ");
-    scanf("%d", &hra);
+    hours = total_seconds / 3600;
+    minutes = (total_seconds % 3600) / 60;
+    seconds = total_seconds % 60;
 
-    printf("Enter DA: ");
-    scanf("%d", &da);
-
-    printf("Enter Other Allowance: ");
-    scanf("%d", &allowance);
-
-    gross = basic + hra + da + allowance;
-
-    printf("Gross Salary = %d", gross);
-
+    printf("Time = %02d:%02d:%02d\n", hours, minutes, seconds);
     return 0;
 }
+
